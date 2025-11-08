035de46f8b63d7a3a96055e65131601e2e7064fc
Athanas Muinde <athanasmwandi5@gmail.com>
2025-11-08 18:07:57 +0300

    feat: build hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+function Hero() {
+  return (
+    <>
+      <div>
+        <h1>Hi I'm Alfred</h1>
+        <p>
+          IT Support Specialist & Software Developer Experienced in providing
+          comprehensive technical support and building modern web applications.
+          Skilled in troubleshooting, system maintenance, and full-stack
+          development with a focus on delivering reliable solutions.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Hero;
